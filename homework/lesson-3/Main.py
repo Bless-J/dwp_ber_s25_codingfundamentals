d9f0a1407540cc3fe92b2d7b7805397c3d6d9383
Bless-J <justineblessing410@gmail.com>
2025-03-27 15:42:43 +0000

    lesson 3 assignment

---
+++ homework/lesson-3/Main.py
@@ -0,0 +1,68 @@
+# firstnumber=5
+# secondnumber=2
+# result= firstnumber + secondnumber
+# print(result) - Addition
+
+# firstnumber=5
+# secondnumber=2
+# print (firstnumber - secondnumber) - Subtraction
+
+# result = 5 * 2
+# print(result)  - Multiplication
+
+# firstnumber = 5
+# result = firstnumber / 2
+# print(result) - Division
+
+# number = 7%3
+# print(number) - Modulus
+
+# number = 7**2
+# print(number) - Raised to power
+
+
+#  Check if the number is even or odd using the modulus operator
+# number = int(input("Enter a number: "))
+# if number % 2 == 0:
+#     print(f"The number is: {number}")
+#     print("The number is even.")
+# else:
+#     print(f"The number is: {number}")
+#     print("The number is odd.")
+
+# Compare the numbers and print the result
+# Number1 =  10
+# Number2 = 20
+# if Number1 > Number2:
+#     print("Number1 is greater")
+# elif Number1 < Number2:
+#     print("Number2 is greater")
+# else:
+#     print("The two numbers are equal.")
+
+# for i in range(1, 11):
+#     print(i) - for loop
+
+# Print multiplication table 
+# number = 3
+# print(f"The number is: {number}")
+# for i in range(1, 11):
+#     print(f"{number} x {i} = {number * i}")
+
+# for i in range(1, 21):
+#     if i % 3 == 0 and i % 5 == 0:
+#         print("FizzBuzz")
+#     elif i % 3 == 0:
+#         print("Fizz")
+#     elif i % 5 == 0:
+#         print("Buzz")
+#     else:
+#         print(i) - # Print numbers from 1 to 20 with FizzBuzz
+
+# Check if the year is a leap year
+
+# year = int(input("Enter a year: "))
+# if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+#     print(f"{year} is a leap year.")
+# else:
+#     print(f"{year} is not a leap year.") used 1980 - it is a leap year.
---
+++ homework/lesson-3/New.py
@@ -0,0 +1,44 @@
+# If, Elif and Else Statements
+# Score = 99
+# if Score > 90:
+#     print ("Grade A")
+# else:
+#     print ("You need to work better next time")
+
+# Value =  85
+# if Value >80:
+#     print("Grade B")
+# elif Value <=90:
+#     print("Grade B")
+# else:
+#     print("Below Grade B")
+
+# Score = 80
+# if Score >60:
+#     print("Grade C")
+# elif Score <=80:
+#     print("Grade C")
+# else:
+#     print("Below Grade C")
+
+
+#While, For, in
+# Number = 1
+# while Number <=100:
+#     print(Number)
+#     Number = Number + 1
+
+#For in range
+Number =["1","101","2"]
+# for Number in range (1, 101, 2):
+#     print(Number)
+
+#Input Function
+# My_name = input ("what is your name?")
+# print("Hello",My_name) #Output will be Hello Blessing
+
+Number1 = int(input("Give me a number"))
+Number2 = int(input("Give me a second number"))
+
+result = Number1 + Number2
+print("Hello, you are hot at",result)
